fd0f2fac985e27021c5fa2b05653ac6a467b8fe6
Nick Bernd Espig <info@nachtalb.io>
2019-12-10 18:18:30 +0100

    Provide precompiled bundle

---
+++ build.js
@@ -0,0 +1,8 @@
+({
+    baseUrl: "./ftw/htmlblock/browser/resources",
+    paths: {
+        jquery: "empty:",
+    },
+    name: "main",
+    out: "./ftw/htmlblock/browser/resources/htmlblock-compiled.js"
+})
